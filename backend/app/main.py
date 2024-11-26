da1ec7de3c155f89d3b8f654a23ee1914c591768
surijatin <surijatin16@gmail.com>
2024-11-26 01:25:16 -0500

    Comment saving CSSV

--- backend/app/main.py
+++ backend/app/main.py
@@ -55,7 +55,7 @@ async def find_people(request: PeopleSearchRequest):
 
     # Perform the LinkedIn staff search
     contacts_df = search_linkedin_staff(job_info, max_results=10)
-    contacts_df.to_csv("contacts_df.csv", index=False)
+    # contacts_df.to_csv("contacts_df.csv", index=False)
     formatted_profiles = format_profile_data(contacts_df)
 
     # Score the profiles based on the job information
